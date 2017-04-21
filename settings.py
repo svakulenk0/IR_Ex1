abc731dff8f5c7879041d45ff44e96aea9a30c20
SV <sv@SVs-MacBook-Air.local>
2017-04-21 14:02:25 +0200

    new: file to store shared variables

---
+++ settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on Apr 20, 2017
+
+.. codeauthor: svitlana vakulenko
+    <svitlana.vakulenko@gmail.com>
+
+'''
+
+# store shared variables here
+TREC8_PATH = 'TREC8all/Adhoc'
+INDEX_PATH = 'index_TREC8'
