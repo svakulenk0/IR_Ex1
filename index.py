0bda084a00b6d3448a813807058b45bcfb81ea94
SV <sv@SVs-MacBook-Air.local>
2017-04-24 15:09:22 +0200

    new: store df counts and number of docs in the inverted index

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ build an inverted index to search document collection.
 '''
 import os
 import pickle
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 import re
 # from nltk.tokenize import word_tokenize
@@ -34,7 +34,7 @@ class Index(object):
     def __init__(self, index_path=False, lower=True, stem=False, lem=True, minToken=4):
         self.index = defaultdict(list)
         self.inv_index = defaultdict(list)
-        self.tokenizer = RegexpTokenizer(r'\w+')
+        self.tokenizer = RegexpTokenizer(r'\w+')  # word-level tokenizer
         self.lower = lower
         self.stem = stem
         self.lem = lem
@@ -58,52 +58,75 @@ class Index(object):
     def preprocess(self, tokens):
         '''
         Preprocess the input document text to normalize the vocabulary
+
+        returns token counter (dictionary with counts for each token)
         '''
         if self.lower:
             tokens = [token.lower() for token in tokens]
         if self.minToken:
             tokens = [token for token in tokens if len(token) >= self.minToken]
-        # TODO stem and lem
-        return tokens
+        # TODO stem and lem ?
+        return Counter(tokens)
+
+    def create_index(self, path=None, limit=None, list_of_strings=None):
+        # counter for documents
+        self.docid = 0
+        if path:
+            self.load_collection(path, limit, inverted=True)
+        else:
+            self.parse_strings(list_of_strings)
+        # calculate document frequencies for each term in inverted index
+        self.df()
+        # store the number of docs in the collection N
+        self.inv_index['N_DOCS'] = self.docid
+        # print self.inv_index
 
     def load_collection(self, path, limit, inverted=True):
         '''
         Load document collection, e.g. CD4&5 of the TIPSTER collection.
         '''
-        # counter for collection documents
-        docid = 0
         for root, dirs, files in os.walk(path):
             for file in files:
-                if docid < limit:
+                if self.docid < limit:
                     with open(os.path.join(root, file), "r") as doc:
                         text = doc.read()
                         text = self.parse_trec(text)
-                        docid += 1
-                        self.create_index(text, docid, inverted=True)
+                        self.docid += 1
+                        self.add_to_index(text, inverted=True)
 
     def parse_trec(self, text):
         '''Removes XML tags from the document text'''
         return re.sub('<[^>]*>', '', text)
 
     def parse_strings(self, list_of_strings):
-        docid = 0
         for string in list_of_strings:
-            docid += 1
-            self.create_index(string, docid)
-
-    def create_index(self, text, docid, inverted=True):
+            self.docid += 1
+            self.add_to_index(string)
+       
+    def add_to_index(self, text, inverted=True):
         '''
-        Builds an inverted index
+        Builds an inverted index with tf(t,d) = f(t,d)
         '''
         # for i, text in enumerate(docs):
         tokens = self.tokenize(text)
-        tokens = self.preprocess(tokens)
-        print 'Document', docid, ':', tokens[:10]
-        # bow model
-        self.index[docid] = tokens
+        token_counts = self.preprocess(tokens)
+        print 'Document', self.docid, 'representation :', token_counts
         if inverted:
-            for token in tokens:
-                self.inv_index[token].append(docid)
+            for token, count in token_counts.items():
+                if token not in self.inv_index:
+                    self.inv_index[token] = {}
+                # tf = f
+                self.inv_index[token][self.docid] = count
+        else:
+            self.index[self.docid] = tokens
+
+    def df(self):
+        '''
+        Calculate and store document frequencies for each term at 'df' indices in the iverted index.
+        '''
+        for token, doc_tfs in self.inv_index.items():
+            print doc_tfs
+            self.inv_index[token]['df'] = sum([tf for doc, tf in doc_tfs.items()])
 
     def store_inv_index(self, index_path):
         '''
@@ -119,11 +142,12 @@ class Index(object):
         with open(index_path, 'rb') as f:
             self.inv_index = pickle.load(f)
 
-    def search(self, query, AND=True):
+    def search(self, query, AND=False):
         '''
         Method to search documents in the inverted index
-        given a query
-
+        given a query: 
+        1) return all the documents where the query terms appear (with the OR operator)
+        2) rank each of the docs using the chosen weighting scheme
         query <String>
         '''
         # preprocessing
@@ -134,22 +158,30 @@ class Index(object):
         doc_has_word = [ (self.inv_index[word], word) for word in wordlist ]
         print doc_has_word
 
-        # if AND : AND query scenario
         # answer_set = set(doc_has_word[0][0])
-        # for d, w in doc_has_word:
-        #     answer_set = answer_set & set(d)
 
+        # for d, w in doc_has_word:
+        #     # AND query scenario
+        #     if AND:
+        #         answer_set = answer_set & set(d)
+        #     # OR query
+        #     else:
+        #         answer_set = answer_set | set(d)
         # return answer_set
 
 
+def parse_trec8():
+    # create new index of the TREC8 collection
+    index = Index()
+    # load collection and store into index
+    index.create_index(path=settings.TREC8_PATH, limit=20)
+    index.store_inv_index(settings.INDEX_PATH)
+
+
 if __name__ == '__main__':
-    # use the index of the TREC8 collection
+    # use the pre-computed index of the TREC8 collection
     index = Index(settings.INDEX_PATH)
 
-    # load collection and store into index
-    # index.load_collection(settings.TREC8_PATH, limit=20)
-    # index.store_inv_index(settings.INDEX_PATH)
-
     # search index
-    query = 'medications 0947'  # answer: 1
+    query = 'medications 0947'
     print index.search(query)
