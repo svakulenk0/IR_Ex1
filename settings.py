a3a0c1669e4d82bf9c5ee854e8e2db10b43c8117
SV <sv@SVs-MacBook-Air.local>
2017-04-25 18:33:57 +0200

    new: data structures for unittests

--- settings.py
+++ settings.py
@@ -12,5 +12,7 @@ Created on Apr 20, 2017
 # store shared variables here
 TREC8_DOCS_PATH = 'TREC8all/Adhoc'
 TREC8_TOPICS_PATH = 'TREC8all/topicsTREC8Adhoc.txt'
+TEST_INDEX_PATH = 'test_index_TREC8'
 INDEX_PATH = 'index_TREC8'
+TEST_LENGTH_PATH = 'test_length_TREC8'
 LENGTH_PATH = 'length_TREC8'
