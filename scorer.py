9f2e5470c8fd833b554e07c7adbf92dbcd65afcc
SV <sv@p237-032.vps.tuwien.ac.at>
2017-04-26 22:26:51 +0200

    new: BM25_ADPT scorer

--- scorer.py
+++ scorer.py
@@ -12,6 +12,11 @@ Various weighting (scoring) functions for search
 '''
 
 import math
+import numpy
+
+from collections import Counter
+
+from scipy.optimize import least_squares, minimize
 
 
 class Scorer(object):
@@ -31,24 +36,34 @@ class Scorer(object):
     #     return tokenized_document.count(term)
         # if posting:
         # print posting
+
+        # calculate stats for the new term
+        # process term stats: idfs, tfqw and k1
+        if term not in self.idfs.keys():
+            # print term
+            self.idfs[term] = self.idf(posting['df'], n_docs)
+            # compute only for BM25 weighting
+            if isinstance(self, BM25) | isinstance(self, BM25_ADPT):
+                # term frequency of the term in the query
+                self.tfqw[term] = tfq / float(self.k3 + tfq)
+                if isinstance(self, BM25_ADPT):
+                # # # term frequency of the term in the query
+                # # self.tfqw[term] = tfq / float(self.k3 + tfq)
+                #     # calculate term-specific k1
+                    self.k1[term] = self.estimate_k1(posting, n_docs)
+                    print term, self.k1[term]
+
         for docid, tf in posting.items():
             # skip non-document placeholder for df
             if docid == 'df':
                 continue
-            # calculate stats for the new term
-            if term not in self.idfs.keys():
-                self.idfs[term] = self.idf(posting['df'], n_docs)
-                # compute only for BM25 weighting
-                if isinstance(self, BM25):
-                    # term frequency of the term in the query
-                    self.tfqw[term] = tfq / float(self.k2 + tfq)
                 # print self.idfs[term]
             # add new document to the ranking
             if docid not in self.ranking.keys():
                 # initial tfidf score for every document
                 self.ranking[docid] = 0
                 # compute only for BM25 weighting
-                if isinstance(self, BM25):
+                if isinstance(self, BM25) | isinstance(self, BM25_ADPT):
                     self.ldws[docid] = lds[docid] / lds['AVG_LD']
             self.ranking[docid] += self.calculate_score(tf, term, docid)
             # print self.ranking[docid]
@@ -77,20 +92,20 @@ class TFIDF(Scorer):
 
 class BM25(Scorer):
     '''
+    k1, k3 and b are the hyper-parameters
     '''
     name = 'bm25'
 
-    def __init__(self, k2=100, k1=1.2, b=0.75):
+    def __init__(self, k3=1000, k1=1.2, b=0.75):
         # weighted term frequencies for each term in the query
         self.tfqw = {}
         self.k1 = k1
         self.b = b
-        self.k2 = k2
+        self.k3 = k3
         self.ldws = {}
 
     def calculate_score(self, tf, term, docid):
         '''
-        k1, k2 and b are the hyper-parameters
         '''
         # print self.tfqw[term]
         # weighted term frequencies for each term in the query
@@ -98,14 +113,64 @@ class BM25(Scorer):
         return self.tfqw[term] * tf / (self.k1 * (1 - self.b + self.b * self.ldws[docid]) + tf) * self.idfs[term]
 
 
-class NewBM25(BM25):
+class BM25_ADPT(Scorer):
     '''
-    Modified BM25 formula as described in the article published by Arjien P. de Vries et al. 2005 at SIGIR
-    (http://homepages.cwi.nl/~arjen/pub/f330-devries.pdf)
+    Modified BM25 formula as described in the article Yuanhua Lv and Cheng Xiang Zhai. Adaptive Term Frequency Normalization for BM25. CIKM 2011
+    (http: //sifaka.cs.uiuc.edu/~ylv2/pub/cikm11-adptTF.pdf)
+    k3 and b are the hyper-parameters
+    k1 is estimated on-line
     '''
 
-    def weight(self):
-        pass
+    name = 'bm25_adpt'
 
-# if __name__ == '__main__':
-    
+    def __init__(self, k3=1000, b=0.75):
+        # weighted term frequencies for each term in the query
+        self.tfqw = {}
+        # term-specific k1
+        self.k1 = {}
+        # self.k1 = {}
+        self.b = b
+        self.k3 = k3
+        self.ldws = {}
+
+    def info_gain(self, dft_1, dft, df, n_docs):
+        '''
+        calculate information gain
+        '''
+        # print (float(dft_1 + 0.5) / (dft + 1))
+        # print (float(df + 0.5) / (n_docs + 1))
+        return math.log((float(dft_1 + 0.5) / (dft + 1)), 2) - math.log((float(df + 0.5) / (n_docs + 1)), 2)
+
+    def k1_fit(self, k1, i, ig):
+        return ig - (k1 + 1) * i / (k1 + i)
+
+    def estimate_k1(self, posting, n_docs):
+        # for docid, tf in posting.items():
+        # tfs = dict(posting)
+        # print posting
+        df = posting['df']
+        del posting['df']
+        dfts = Counter(posting.values())
+        # print dfts
+        # print dfts.keys()
+        k1_sum = 0
+        # print dfts[2], dfts[1], df, n_docs
+        ig1 = self.info_gain(dfts[2], dfts[1], df, n_docs)
+        # print ig1
+        i_s = []
+        igs = []
+        for i in range(0, max(dfts.keys())+1):
+            # print i
+            # print dfts[i+1], dfts[i]
+            igt = self.info_gain(dfts[i+1], dfts[i], df, n_docs)
+            # print igt / ig1 - (k1 + 1) * i / (k1 + 1)
+            i_s.append(i)
+            igs.append(igt/ig1)
+        out = least_squares(self.k1_fit, 1.2, args=(numpy.array(i_s), numpy.array(igs)))
+        return out.x[0]
+
+    def calculate_score(self, tf, term, docid):
+        '''
+        '''
+        return self.tfqw[term] * tf / (self.k1[term] * (1 - self.b + self.b * self.ldws[docid]) + tf) * self.idfs[term]
+        # return self.tfqw[term] * tf / (self.k1[term] * (1 - self.b + self.b * self.ldws[docid]) + tf) * self.idfs[term]
