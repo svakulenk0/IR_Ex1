45473ebf1ab02d30da7f33210532cc80f79018ee
SV <sv@p236-241.vps.tuwien.ac.at>
2017-04-27 12:03:44 +0200

    new: function to convert strings to boolean values

--- settings.py
+++ settings.py
@@ -16,3 +16,12 @@ TEST_INDEX_PATH = 'test_index_TREC8'
 INDEX_PATH = 'index_TREC8'
 TEST_LENGTH_PATH = 'test_length_TREC8'
 LENGTH_PATH = 'length_TREC8'
+
+# shared method to convert strings to Python boolean type for argparse
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    if v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
